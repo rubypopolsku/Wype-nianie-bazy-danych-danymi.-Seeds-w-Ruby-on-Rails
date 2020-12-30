1bac39ea8e2cc95e4bd130d80ac78a5d889cf96a
Tobiasz Waszak <tobiaszwaszak@gmail.com>
2020-12-30 13:28:31 -0800

    bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+var jQuery = require("jquery")
+
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery'
+
+require('bootstrap')
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()

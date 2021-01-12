dae2572bb9ca6af465806cfeec35e62e139f7233
Tobiasz Waszak <tobiaszwaszak@gmail.com>
2021-01-12 10:10:16 -0800

    walidacje

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
     has_many :comments
     belongs_to :category, optional: true
     belongs_to :user
+
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
 end

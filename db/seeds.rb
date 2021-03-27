98d23cba727f667595b7c73096d1cc6742689a3b
Tobiasz Waszak <tobiaszwaszak@gmail.com>
2021-03-27 20:27:52 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+autor = User.create(email: "kontakt@rubypopolsku.pl", password: "foobar2000", author: true)
+komentujacy = User.create(email: "foo@bar.dev", password: "Qwerty123", author: false)
+
+2.times do
+  Category.create(name: Faker::Books::Lovecraft.fhtagn)
+end
+
+10.times do
+  Post.create(
+    title: Faker::Books::Lovecraft.sentence,
+    body: Faker::Books::Lovecraft.paragraph(sentence_count: 5),
+    category: Category.all.sample,
+    user: autor
+  ).tap do |post|
+    2.times do
+      Comment.create(body: Faker::TvShows::Friends.quote, post: post, user: User.all.sample)
+    end
+  end
+end
